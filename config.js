75eaecdfbe006018da4014ca59e056a88c424c0d
Jeremy Dyer <jdye64@gmail.com>
2015-02-04 10:18:50 -0500

    S3 location for audio files changed to new location Tom wants to use at https://s3.amazonaws.com/makeandbuild-courier/

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ config.socketio.port = process.env.WEB_PORT || 8081;
 
 //Configuration for the Raspberry PI audio
 config.audio.rpi.cachedir = "/home/pi/.audio/";
-config.audio.rpi.s3url = "https://s3.amazonaws.com/makeandbuild/courier/audio/";
+config.audio.rpi.s3url = "https://s3.amazonaws.com/makeandbuild-courier/";
 
 //TODO need to find the value nodejs provides for raspberry pi os.platform()
 config.audio.players = {"darwin": {"mp3": "afplay", "wav": "afplay"}, "linux": {"mp3": "mpg123", "wav": "aplay"}};
